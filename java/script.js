9ae3ffeb0ee3a3582e6911c703e42dedd149c309
Nick Bledsoe <nbledsoe@hawk.iit.edu>
2019-02-07 01:55:28 -0600

    pushing a nights worth of work

--- java/script.js
+++ java/script.js
@@ -1,6 +1,9 @@
-  function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+public class JavaScript
+  public static void main(String[] args) {
+    function openForm() {
+      document.getElementById("myForm").style.display = "block";
+    }
+    function closeForm() {
+      document.getElementById("myForm").style.display = "none";
+    }
   }
