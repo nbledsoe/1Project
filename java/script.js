cc9da0e46019461c3ae26ced8d8d7432e6889dcb
Nick Bledsoe <nbledsoe@hawk.iit.edu>
2019-02-28 16:08:25 -0600

    changing it so it points to email

--- java/script.js
+++ java/script.js
@@ -1,9 +1,7 @@
-public class JavaScript
-  public static void main(String[] args) {
+
     function openForm() {
       document.getElementById("myForm").style.display = "block";
     }
     function closeForm() {
       document.getElementById("myForm").style.display = "none";
     }
-  }
