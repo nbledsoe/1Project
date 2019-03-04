668567b88f184c9b13c580f16a8643d6f99ad70e
Nick Bledsoe <nbledsoe@hawk.iit.edu>
2019-03-04 00:23:01 -0600

    dont know how to change display to block not hidden

--- java/script.js
+++ java/script.js
@@ -1,10 +1,4 @@
 (function(){
-    function openForm() {
-      document.getElementById("myForm").style.display = "block";
-    }
-      function closeForm() {
-      document.getElementById("myForm").style.display = "none";
-    }
-  document.getElementById("myForm").addEventListener("click" ());
+  document.getElementById("myForm").addEventListener("click", function());
 
 }());
