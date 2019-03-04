84b458c315e3f1aac50379738b310a722b161c6e
Nick Bledsoe <nbledsoe@hawk.iit.edu>
2019-03-04 00:34:27 -0600

    Added this to top

--- java/script.js
+++ java/script.js
@@ -1,4 +1,8 @@
 (function(){
+  if (!('querySelector' in document && 'addEventListener' in document)) {
+    return;
+  }
+
   document.getElementById("myForm").addEventListener("click", function());
 
 }());
