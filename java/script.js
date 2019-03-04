35cab6c2d92a7ea7a92548b5fd9e083fb51f8200
Nick Bledsoe <nbledsoe@hawk.iit.edu>
2019-03-03 23:38:21 -0600

    pushing new java script

--- java/script.js
+++ java/script.js
@@ -1,7 +1,11 @@
-
+(function(){
     function openForm() {
       document.getElementById("myForm").style.display = "block";
     }
     function closeForm() {
       document.getElementById("myForm").style.display = "none";
     }
+    document.addEventListener("click", openForm()){
+    }
+
+}());
